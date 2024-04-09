5b9f7ca66d0a313830dae91ecc64e41439a44571
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2024-04-09 16:18:18 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,23 @@ if (button && input) {
         concatenation(input.value, 'hello!');
     });
 }
+function compare(top, bottom) {
+    return {
+        name: top.name,
+        color: top.color,
+        position: bottom.position,
+        weight: bottom.weight,
+    };
+}
+let first = { name: 'Alice',
+    position: 10,
+    color: 'Yello',
+    weight: 15,
+};
+let second = { name: 'Cooper',
+    position: 12,
+    color: 'Green',
+    weight: 20,
+};
+compare(first, second);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,34 @@ if (button && input) {
   });
 }
 
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number
+}
 
+function compare <T extends AllType, K extends AllType> (top: Pick <T, keyof AllType>, bottom: Pick <K, keyof AllType>): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
+}
+
+let first = {name: 'Alice',
+  position: 10,
+  color: 'Yello',
+  weight: 15,
+}
+
+let second = {name: 'Cooper',
+  position: 12,
+  color: 'Green',
+  weight: 20,
+}
+
+compare(first, second);
+
+export {};
